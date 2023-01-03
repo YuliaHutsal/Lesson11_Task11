ab9ec8b5f3cf98e9658d7db092df622ba61f1db2
Yulia Gutsal <julik.ua@gmail.com>
2023-01-03 18:00:53 +0200

    create Dialog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import Modal from './Components/Modal';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Root = styled.div`
+  font-family:sans-serif;
+  text-align: center;
+  position:relative;
+`
+const Overlay = styled.div`
+position:fixed;
+top:0;
+left:0;
+right:0;
+bottom:0;
+background-color:rgba(0, 0, 0, .5);
+`
+const Dialog = styled.div`
+background:#fff;
+border-radius: 5px;
+padding:20px;
+position:absolute;
+z-index: 1;
+top: 350px;
+left: 700px;
+`
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Root>
+      <h1>Modal components</h1>
+      <h2>Example</h2>
+      <button onClick={() => setIsModalOpen(true)}>Open window</button>
+      {
+        isModalOpen && (
+          <Overlay>
+            <Dialog>
+              <p>
+                Lorem ipsum,
+              </p>
+              <button onClick={() => {setIsModalOpen(false)}}>Close window</button>
+            </Dialog>
+          </Overlay>
+        )
+      }
+    </Root>
   );
 }
 
 export default App;
+
